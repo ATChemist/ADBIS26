e16d43396b89d9959ac72b0ec85030c538004271
Frederik Ludvig Abel Tiklev <Frederik.a.tiklev@gmail.com>
2026-03-02 20:04:37 +0100

    frisk

--- src/views/components/ui/Skeleton.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from '../../../utils/cn';
-
-interface SkeletonProps {
-  className?: string;
-}
-
-export function Skeleton({ className }: SkeletonProps) {
-  return <div className={cn('animate-pulse rounded-xl bg-slate-200/80', className)} aria-hidden />;
-}
